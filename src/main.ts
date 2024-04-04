cd54ce668cc9e45bfc8e2938cce58e6d52a5c0dc
Austin Merrick <austinmerrick@onsclom.net>
2024-04-03 22:34:16 -0700

    Figure out basic tree walking

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,24 @@
-console.log("working!");
+import { parse } from "acorn";
+import { full } from "acorn-walk";
+
+const textInput = document.createElement("input");
+textInput.type = "text";
+document.body.appendChild(textInput);
+const astList = document.createElement("ul");
+document.body.appendChild(astList);
+
+textInput.value = "1 + 2 * 3 - 4";
+
+function update() {
+  astList.innerHTML = "";
+  const ast = parse(textInput.value, { ecmaVersion: 2020 });
+  const ulStack = [astList];
+  full(ast, (node) => {
+    // if (node.type !== "BinaryExpression") return;
+    const li = document.createElement("li");
+    li.textContent = `${node.type}: (${node.start}-${node.end})`;
+    ulStack[ulStack.length - 1].appendChild(li);
+  });
+}
+update();
+textInput.addEventListener("input", update);
